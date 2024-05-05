2679554375323167456c375fe2dc38628f6abea0
StatistIRL <vazsry7@gmail.com>
2024-05-05 18:10:54 +0600

    start dev base.html ; add js; before django_compressor

---
+++ burger/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def main_page(request):
+
+    context = {}
+    return render(request, "burger/index.html", context=context)
--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # APPS
+    "burger",
     # MODULES
     "debug_toolbar",
     "django_extensions",
